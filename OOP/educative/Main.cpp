9355a4ecb3c8ee114b32e1be7ca874ce72a5e8b9
Shahzaib Hassan <shahxeebhassan@gmail.com>
2022-08-08 10:28:48 +0500

    Create Main.cpp

---
+++ OOP/educative/Main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int calculateSum(int p1, int p2){
+  int sum = p1 + p2;
+  return sum;
+}
+
+int main() {
+  int p1,p2;
+  cout<<"Enter number 1: \n";
+  cin>>p1;
+  cout<<"Enter number 2: \n";
+  cin>>p2;
+  
+  cout<<calculateSum(p1,p2);  // calculates sum of given numbers
+  return 0;
+}
