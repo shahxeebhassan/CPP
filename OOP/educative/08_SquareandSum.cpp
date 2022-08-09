e24a84cb26f9df9f9e195612f1ac95272cfe2f76
Shahzaib Hassan <shahxeebhassan@gmail.com>
2022-08-09 16:35:01 +0500

    Create 08_SquareandSum.cpp

---
+++ OOP/educative/08_SquareandSum.cpp
@@ -0,0 +1,10 @@
+float squareSum(float num1, float num2, float num3) {
+  float sum = 0;
+  
+  // write your code here
+  num1*=num1;
+  num2*=num2;
+  num3*=num3;
+  sum= num1+num2+num3;
+  return sum;
+}
